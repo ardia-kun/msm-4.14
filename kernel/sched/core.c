8ded4078013fbcf068603c1ac4f681ddaf36f84e
Thomas Gleixner <tglx@linutronix.de>
2013-01-11 11:23:51 +0100

    completion: Use simple wait queues
    Completions have no long lasting callbacks and therefor do not need
    the complex waitqueue variant. Use simple waitqueues which reduces the
    contention on the waitqueue lock.
    
    Change-Id: I071e1408cb43f2b432179c42b01ced8df6efd127
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: Ardia-kun <85530328+ardia-kun@users.noreply.github.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7843,7 +7843,10 @@ void migrate_disable(void)
 		return;
 	}
 #ifdef CONFIG_SCHED_DEBUG
-	WARN_ON_ONCE(p->migrate_disable_atomic);
+	if (unlikely(p->migrate_disable_atomic)) {
+		tracing_off();
+		WARN_ON_ONCE(1);
+	}
 #endif
 
 	if (p->migrate_disable) {
@@ -7873,7 +7876,10 @@ void migrate_enable(void)
 	}
 
 #ifdef CONFIG_SCHED_DEBUG
-	WARN_ON_ONCE(p->migrate_disable_atomic);
+	if (unlikely(p->migrate_disable_atomic)) {
+		tracing_off();
+		WARN_ON_ONCE(1);
+	}
 #endif
 
 	WARN_ON_ONCE(p->migrate_disable <= 0);

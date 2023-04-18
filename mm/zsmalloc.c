e10ca9ff70f97acfd8afe8b31bcaff54635c6f25
Sergey Senozhatsky <senozhatsky@chromium.org>
2023-04-18 16:46:39 +0900

    zsmalloc: allow only one active pool compaction context
    zsmalloc pool can be compacted concurrently by many contexts,
    e.g.
    
     cc1 handle_mm_fault()
          do_anonymous_page()
           __alloc_pages_slowpath()
            try_to_free_pages()
             do_try_to_free_pages(
              lru_gen_shrink_node()
               shrink_slab()
                do_shrink_slab()
                 zs_shrinker_scan()
                  zs_compact()
    
    Pool compaction is currently (basically) single-threaded as
    it is performed under pool->lock. Having multiple compaction
    threads results in unnecessary contention, as each thread
    competes for pool->lock. This, in turn, affects all zsmalloc
    operations such as zs_malloc(), zs_map_object(), zs_free(), etc.
    
    Introduce the pool->compaction_in_progress atomic variable,
    which ensures that only one compaction context can run at a
    time. This reduces overall pool->lock contention in (corner)
    cases when many contexts attempt to shrink zspool simultaneously.
    
    Link: https://lkml.kernel.org/r/20230418074639.1903197-1-senozhatsky@chromium.org
    Fixes: c0547d0b6a4b ("zsmalloc: consolidate zs_pool's migrate_lock and size_class's locks")
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Reviewed-by: Yosry Ahmed <yosryahmed@google.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Ardia-kun <85530328+ardia-kun@users.noreply.github.com>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -279,6 +279,7 @@ struct zs_pool {
 	struct inode *inode;
 	struct work_struct free_work;
 #endif
+	atomic_t compaction_in_progress;
 };
 
 struct zspage {
@@ -2348,6 +2349,15 @@ unsigned long zs_compact(struct zs_pool *pool)
 	struct size_class *class;
 	unsigned long pages_freed = 0;
 
+	/*
+	 * Pool compaction is performed under pool->lock so it is basically
+	 * single-threaded. Having more than one thread in __zs_compact()
+	 * will increase pool->lock contention, which will impact other
+	 * zsmalloc operations that need pool->lock.
+	 */
+	if (atomic_xchg(&pool->compaction_in_progress, 1))
+		return 0;
+
 	for (i = ZS_SIZE_CLASSES - 1; i >= 0; i--) {
 		class = pool->size_class[i];
 		if (!class)
@@ -2357,6 +2367,7 @@ unsigned long zs_compact(struct zs_pool *pool)
 		pages_freed += __zs_compact(pool, class);
 	}
 	atomic_long_add(pages_freed, &pool->stats.pages_compacted);
+	atomic_set(&pool->compaction_in_progress, 0);
 
 	return pages_freed;
 }
@@ -2467,6 +2478,7 @@ struct zs_pool *zs_create_pool(const char *name)
 		return NULL;
 
 	spin_lock_init(&pool->lock);
+	atomic_set(&pool->compaction_in_progress, 0);
 
 	init_deferred_free(pool);
 

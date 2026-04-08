178ce5a92148d1edb84d921660dbb6811f4fc43f
Ardia-kun <85530328+ardia-kun@users.noreply.github.com>
2026-04-08 14:33:47 +0000

    power: qcom: qg-soc: fix stray preprocessor directives

--- drivers/power/supply/qcom/qg-soc.c
+++ drivers/power/supply/qcom/qg-soc.c
@@ -446,12 +446,9 @@ static void update_msoc(struct qpnp_qg *chip)
 		/* SOC dropped */
 		if (batt_cur > 0) {
 			chip->msoc -= chip->dt.delta_soc;
-		}
-#else
-		if (!input_present) {
+		} else if (!input_present) {
 			chip->msoc -= chip->dt.delta_soc;
 		}
-#endif
 	}
 	chip->msoc = CAP(0, 100, chip->msoc);
 
